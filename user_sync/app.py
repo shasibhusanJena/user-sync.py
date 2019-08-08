d5aa2a6d1ea7172b97bba540c43dae4682bbb1d5
Andrew Dorton <adorton@adobe.com>
2019-08-08 14:50:54 -0600

    fix config loading and Sign API access

--- user_sync/app.py
+++ user_sync/app.py
@@ -164,8 +164,8 @@ def main():
 def sync(**kwargs):
     """Run User Sync [default command]"""
     run_stats = None
-    sign_config_file = kwargs.get('sign-sync-config')
-    if 'sign-sync-config' in kwargs:
+    sign_config_file = kwargs.get('sign_sync_config')
+    if 'sign_sync_config' in kwargs:
         del(kwargs['sign_sync_config'])
     try:
         # load the config files and start the file logger
@@ -365,6 +365,7 @@ def begin_work(config_loader, sign_config_file):
     if sign_config_file:
         # Need to sleep the application before performing the sync. This is due to the fact that it takes around
         # 30-45 secs for the users to populate into sign.
+        logger.info('running Sign sync')
         time.sleep(60)
         sign_sync.run(config_loader, rule_processor.updated_user_keys, sign_config_file)
 
