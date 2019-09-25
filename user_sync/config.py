c8bc374d96911481e92746823f95f8fcd0c5384d
michaelmernin <michael.mernin@gmail.com>
2019-09-25 16:56:13 -0500

    dynamic console mapping added

--- user_sync/config.py
+++ user_sync/config.py
@@ -464,8 +464,11 @@ class ConfigLoader(object):
         additional_groups = directory_config.get_list('additional_groups', True) or []
         try:
             additional_groups = [{'source': re.compile(r['source']),
-                                  'target': user_sync.rules.AdobeGroup.create(r['target'], index=False)}
+                                  'target': r['target']}
                                  for r in additional_groups]
+            # additional_groups = [{'source': re.compile(r['source']),
+            #                       'target': user_sync.rules.AdobeGroup.create(r['target'], index=False)}
+            #                      for r in additional_groups]
         except Exception as e:
             raise AssertionException("Additional group rule error: {}".format(str(e)))
         options['additional_groups'] = additional_groups
